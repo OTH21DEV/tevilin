34e07137ae74bac91669ffd50f073b8149d1b212
Oxana Theis <otheis@protonmail.com>
2024-08-08 21:49:00 +0100

    finish table page

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,21 +1,22 @@
 export function displayFooter() {
-    const sidebarLinks = document.querySelectorAll(".sidebar__link");
-    const footer = document.querySelector(".footer");
-  
-    // Function to set footer content based on target type
-    function setFooterContent(target) {
-      // Removes any existing footer sections if they exist
-      const existingSections = footer.querySelectorAll(".footer__section");
-      existingSections.forEach((section) => {
-        if (footer.contains(section)) {
-          footer.removeChild(section);
-        }
-      });
-  
-      if (target === "localisation") {
-        const container = document.createElement("div");
-        container.classList.add("footer__section");
-        container.innerHTML = `
+  const sidebarLinks = document.querySelectorAll(".sidebar__link");
+  const footer = document.querySelector(".footer");
+
+  // Function to set footer content based on target type
+  function setFooterContent(target) {
+    // Removes any existing footer sections if they exist
+    const existingSections = footer.querySelectorAll(".footer__section");
+    existingSections.forEach((section) => {
+      if (footer.contains(section)) {
+        footer.removeChild(section);
+      }
+    });
+
+    if (target === "localisation") {
+      const container = document.createElement("div");
+      container.classList.add("footer__section");
+      document.querySelector(".main-content").style.minHeight = "100vh";
+      container.innerHTML = `
           <p class="footer__content">
             Последний приём 20.09.2021 18:36:21
           </p>
@@ -26,30 +27,38 @@ export function displayFooter() {
             <img src="./assets/doc_icon.png" alt="Документация иконка" class="footer__icon">
             <p class="footer__text">Документация</p>
           </div>`;
-        footer.appendChild(container);
-      } else {
-        const otherContainer = document.createElement("div");
-        otherContainer.classList.add("footer__section");
-        otherContainer.innerHTML = `
-          <p class="footer__content">
-            Альтернативный содержимый футер.
-          </p>
+      footer.appendChild(container);
+    } else {
+      const otherContainer = document.createElement("div");
+      otherContainer.classList.add("footer__section");
+      footer.style.justifyContent = "start";
+      footer.style.paddingLeft = "337px";
+      document.querySelector(".main-content").style.minHeight = "";
+      otherContainer.innerHTML = `
+            <button class="btn btn-save-footer">
+          <img src="./assets/save_icon.png" alt="" />
+          Добавить поле
+        </button>
+          <button class="btn btn-cancel-footer">
+          
+          Отменить
+        </button>
           <div class="footer__section footer__documentation">
             <img src="./assets/doc_icon.png" alt="Документация иконка" class="footer__icon">
             <p class="footer__text">Документация</p>
           </div>`;
-        footer.appendChild(otherContainer);
-      }
+      footer.appendChild(otherContainer);
     }
-  
-    // Initial footer content
-    setFooterContent("localisation");
-  
-    sidebarLinks.forEach((link) => {
-      link.addEventListener("click", (event) => {
-        const target = event.currentTarget.dataset.content;
-        setFooterContent(target);
-        event.preventDefault();
-      });
-    });
   }
+
+  // Initial footer content
+  setFooterContent("localisation");
+
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      const target = event.currentTarget.dataset.content;
+      setFooterContent(target);
+      event.preventDefault();
+    });
+  });
+}
